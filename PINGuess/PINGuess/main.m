8fc33c75371ed34ea172152a4ea6879bbc2397ef
rwatalin <rwatalin@mix.wvu.edu>
2016-02-11 19:53:47 -0500

    completed Pinguess assignment RW

--- PINGuess/PINGuess/main.m
+++ PINGuess/PINGuess/main.m
@@ -8,27 +8,36 @@
 
 #import <Foundation/Foundation.h>
 
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         
         NSString *pinNumber = @"5412";
-        //random comment
         
-        /*
-         Use the following line of code to convert an integer into a 4 digit string (where i is the int):
-         
-         NSString *formatted = [NSString stringWithFormat:@"%04d",i];
-         
-         Use the following line of code to check to see if your pin guess is equal to the pinNumber:
-         
-         [pinNumber isEqualToString:formatted]
-         
-        */
+        // for loop you know how many times it can run
+        int i;
+        for (i = 0; i <= 10000; i++)
+        {
             
-        
-        
+        // Use the following line of code to convert an integer into a 4 digit string (where i is the int)
+            
+            NSString *formatted = [NSString stringWithFormat:@"%04d",i];
+            NSLog(formatted);
+            
+            // Use the following line of code to check to see if your pin guess is equal to the pinNumber:
+            
+            if ([pinNumber isEqualToString:formatted]) {
+                NSLog(@"The pin number is 5412");
+                break;
+            }
+            else {
+                NSLog(@"Incorrect pin number");
+            }
+            
+        }
+     
         
     }
     return 0;
-}
+};
