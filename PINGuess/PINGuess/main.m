ebef69bc255442eb05b6c02f63950ffc5bbaac52
jmathen2 <jmathen2@mix.wvu.edu>
2016-02-16 17:13:27 -0500

    "Pin Guess" Assignment
    I tried to make this code so you could input a number to start with
    less than 4 digits and the code would still run. Similar to what the
    left pad function did in the javascript assignment. Not sure if we
    needed to do that or not.

--- PINGuess/PINGuess/main.m
+++ PINGuess/PINGuess/main.m
@@ -10,23 +10,34 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
         
-        NSString *pinNumber = @"5412";
+        // This first NSString creates the pin number as a string
+        NSString *pinNumber = @"602";
+        //This line convert the pinNumber string to an integer and stores it as the new variable integer pin
+        // I didn't know how to correct this warning for turning a long floating point number to an integer
         
-        /*
-         Use the following line of code to convert an integer into a 4 digit string (where i is the int):
-         
-         NSString *formatted = [NSString stringWithFormat:@"%04d",i];
-         
-         Use the following line of code to check to see if your pin guess is equal to the pinNumber:
-         
-         [pinNumber isEqualToString:formatted]
-         
-        */
+        int integerPin = [pinNumber integerValue];
+        // The stringwithformat function then converts the integer to a string again however with zeros added to complete the 4 digit pin
+        // This allows the program to work even if the user inputs less than a complete 4 digit pin guess
+        NSString *adjustedPin = [NSString stringWithFormat:@"%04d",integerPin];
+        NSLog(@"The pin number is %@...",pinNumber);
+        NSLog(@"The adjusted pin is %@...",adjustedPin);
+        
+        // Creating the index variable i for the for loop
+        int i;
+        // For loop used to check different guesses of the pin code from 0 to 9999
+        for (i=0; i<10000; i++) {
             
-        
-        
+            //Line 31 converts the interger index i to a string with 4 digits
+            NSString *formatted = [NSString stringWithFormat:@"%04d",i];
+            
+            //The if statement in lines 36-39 checks to see if the index is the same as the pin code and alerts the user if it is and then breaks the loop
+            //The condition of the if statment uses the isEqualToString function to output a Boolean value as 1 for Yes and 0 for No...
+            if ([adjustedPin isEqualToString:formatted] == 1) {
+                NSLog(@"You have found the pin code...It is %d...",i);
+                break;
+            }
+        }
         
     }
     return 0;
