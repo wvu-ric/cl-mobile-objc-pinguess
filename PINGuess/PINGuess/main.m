08d1b2af5ffd69a87cebfbb1c4db5c485f168d85
AustinRClark <aoclark@mix.wvu.edu>
2016-02-18 19:44:55 -0500

    Guesses pin and if pin does not exists results in error message.

--- PINGuess/PINGuess/main.m
+++ PINGuess/PINGuess/main.m
@@ -14,6 +14,20 @@ int main(int argc, const char * argv[]) {
         
         NSString *pinNumber = @"5412";
         
+        for (int i = 0; i < 10000; i++) {
+            NSString *i_int = [NSString stringWithFormat:@"%d", i];
+            NSString *formatted = [NSString stringWithFormat:@"%04d",i];
+        
+            if ([pinNumber isEqualToString:formatted]) {
+                NSLog(@"Pin found. Found to be %@", formatted);
+                break;
+            }
+            if (![pinNumber isEqualToString:  @"9999"] && [i_int isEqualToString: @"9999"] ) {
+                  NSLog(@"ERROR. PIN NOT FOUND");
+                  break;
+            }
+        
+        }
         /*
          Use the following line of code to convert an integer into a 4 digit string (where i is the int):
          
